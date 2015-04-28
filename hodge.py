c6fff3fd4e7d796aabd51f8da1d8e84cb8a8d05e
Thiago Avelino <thiagoavelinoster@gmail.com>
2015-04-28 01:20:46 -0300

    start newpost method

--- hodge.py
+++ hodge.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
 import click
 from cookiecutter.main import cookiecutter
+from slugify import slugify
+from jinja2 import Environment, PackageLoader
 
 
 @click.group()
@@ -19,12 +22,26 @@ def init(site_name):
         'site_name': site_name,
         "repo_name": repo_name,
         "app_name": app_name,
-        "pkg_name": app_name
+        "pkg_name": app_name,
+        "content_folder": "content"
     }
     cookiecutter(
         'https://github.com/avelino/hodge-init.git',
         extra_context=extra_context,
         no_input=True)
+
+
+@cmds.command()
+def newpost():
+    if not os.path.isfile("./hodge.toml"):
+        click.echo(u'hodge.toml (config) not exist!')
+        exit(0)
+
+    click.echo(u'Hodge new post create...')
+    obj = {}
+    obj["title"] = click.prompt('Title', type=str)
+    obj["slug"] = slugify(obj["title"])
+    obj["tags"] = click.prompt('Tags (hodge, static)', type=str)
 
 
 def main():
---
+++ templates/newpost.md
@@ -0,0 +1,11 @@
+# {{ title }}
+
+- date: 2015-04-27 22:10
+- author: avelino
+- category: hodge
+- tags: {{ tags }}
+- slug: {{ slug }}
+
+-------
+
+Welcome to Hodge.
